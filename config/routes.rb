3ff17cfb1020322016e56aa4120cf08660ed8b29
Karan Mehta <karanm645@gmail.com>
2022-02-08 21:34:40 -0600

    Delete item functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do 
         resources :items, module: 'merchants', only: [:index]
       end 
-      resources :items, only: [:index, :show, :create]
+      resources :items, only: [:index, :show, :create, :destroy]
     end 
   end 
 end
