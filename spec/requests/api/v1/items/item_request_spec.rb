a70325c532719fa8ab42eb57911455897d2a05a0
Karan Mehta <karanm645@gmail.com>
2022-02-10 18:49:38 -0600

    Complete finding all items

--- spec/requests/api/v1/items/item_request_spec.rb
+++ spec/requests/api/v1/items/item_request_spec.rb
@@ -4,73 +4,74 @@ RSpec.describe "item API" do
   before :each do 
     @merchant = create(:merchant)
   end 
-  it "renders all items" do 
-    create_list(:item, 10, merchant: @merchant)
 
-    get '/api/v1/items'
-    expect(response).to be_successful
-    items = JSON.parse(response.body, symbolize_names: true)
-    expect(items[:data].count).to eq(10)
+  describe 'happy paths' do 
+    it "renders all items" do 
+      create_list(:item, 10, merchant: @merchant)
 
-    items[:data].each do |item|
-      expect(item[:attributes]).to have_key(:name)
-      expect(item[:attributes][:name]).to be_a(String)
+      get '/api/v1/items'
+      expect(response).to be_successful
+      items = JSON.parse(response.body, symbolize_names: true)
+      expect(items[:data].count).to eq(10)
 
-      expect(item[:attributes]).to have_key(:description)
-      expect(item[:attributes][:description]).to be_a(String)
+      items[:data].each do |item|
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
 
-      expect(item[:attributes]).to have_key(:unit_price)
-      expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+      end 
     end 
-  end 
 
-  it "can get one item by its id" do 
-    create_list(:item, 10, merchant: @merchant)
-    item = Item.last
+    it "can get one item by its id" do 
+      create_list(:item, 10, merchant: @merchant)
+      item = Item.last
 
-    get "/api/v1/items/#{item.id}"
-    item = JSON.parse(response.body, symbolize_names: true)
-    expect(response).to be_successful
+      get "/api/v1/items/#{item.id}"
+      item = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      
+      expect(item[:data]).to have_key(:id)
+
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes][:name]).to be_a(String)
+      
+      expect(item[:data][:attributes]).to have_key(:description)
+      expect(item[:data][:attributes][:description]).to be_a(String)
+      
+      expect(item[:data][:attributes]).to have_key(:unit_price)
+      expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+    end 
+
+    it "can create a new item" do 
+      create_list(:item, 10, merchant: @merchant)
+      post "/api/v1/items", params: {
+                      name: 'new item',
+                      description: 'new description',
+                      unit_price: 5.5,
+                      merchant_id: @merchant.id
+                    }
     
-    expect(item[:data]).to have_key(:id)
+      item = JSON.parse(response.body, symbolize_names: true)
+      new_item = Item.find(item[:data][:id])
+      expect(response).to be_successful
 
-    expect(item[:data][:attributes]).to have_key(:name)
-    expect(item[:data][:attributes][:name]).to be_a(String)
-    
-    expect(item[:data][:attributes]).to have_key(:description)
-    expect(item[:data][:attributes][:description]).to be_a(String)
-    
-    expect(item[:data][:attributes]).to have_key(:unit_price)
-    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
-  end 
+      expect(item[:data][:attributes][:name]).to eq(new_item[:name])
+      expect(item[:data][:attributes][:description]).to eq(new_item[:description])
+      expect(item[:data][:attributes][:unit_price]).to eq(new_item[:unit_price])
+    end 
 
-  it "can create a new item" do 
-    create_list(:item, 10, merchant: @merchant)
-    post "/api/v1/items", params: {
-                    name: 'new item',
-                    description: 'new description',
-                    unit_price: 5.5,
-                    merchant_id: @merchant.id
-                  }
-  
-    item = JSON.parse(response.body, symbolize_names: true)
-    new_item = Item.find(item[:data][:id])
-    expect(response).to be_successful
+    it "can destroy an item" do 
+      item = create(:item, merchant: @merchant)
 
-    expect(item[:data][:attributes][:name]).to eq(new_item[:name])
-    expect(item[:data][:attributes][:description]).to eq(new_item[:description])
-    expect(item[:data][:attributes][:unit_price]).to eq(new_item[:unit_price])
-  end 
+      expect{ delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
+      expect(response).to be_successful
+      expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end 
 
-  it "can destroy an item" do 
-    item = create(:item, merchant: @merchant)
-
-    expect{ delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
-    expect(response).to be_successful
-    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
-
-  end 
-  describe "update item" do 
     it "can update an existing item" do 
       id = create(:item, merchant: @merchant).id
       previous_name = Item.last.name 
@@ -87,11 +88,36 @@ RSpec.describe "item API" do
       expect(item.name).to_not eq(previous_name)
       expect(item.name).to eq("updated item")
     end 
+  end 
 
-    it "edge case for bad id" do 
-      id = create(:item, merchant: @merchant).id
-      # id = 0
+  describe 'sad path' do 
+
+    it "renders an error message for bad id" do 
+      id = 0
       get "/api/v1/items/#{id}"
+      body = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(response.status).to eq(404)
+    end 
+  end 
+
+  describe 'find all items' do 
+    describe 'happy path' do 
+      it "find all items based on search criteria" do 
+        items = create_list(:item, 10, merchant: @merchant)
+        necklace = create(:item, name: "Necklace", merchant: @merchant)
+        necktie = create(:item, name: "Neck Tie", merchant: @merchant)
+        get "/api/v1/items/find_all?name=neck"
+
+        body = JSON.parse(response.body, symbolize_names: true)
+      end 
+    end 
+  end 
+
+  describe 'sad path' do 
+    it 'cant find a merchant by id' do 
+      id = 0
+      get "/api/v1/merchants/#{id}/items"
       body = JSON.parse(response.body, symbolize_names: true)
       
       expect(response.status).to eq(404)
--- spec/requests/api/v1/merchants/items_request_spec.rb
+++ spec/requests/api/v1/merchants/items_request_spec.rb
@@ -5,30 +5,22 @@ RSpec.describe 'merchant items exposure' do
   describe 'happy path' do 
     it "can get a merchants items" do 
       merchant = create(:merchant)
-      create_list(:item, 10, merchant: merchant)
       items = merchant.items
-      # id = create(:merchant).id
+      create_list(:item, 10, merchant: merchant)
+
       get "/api/v1/merchants/#{merchant.id}/items"
       all_items = JSON.parse(response.body, symbolize_names: true)
-      
       
       expect(response).to be_successful
       expect(items.count).to eq(10)
       
       all_items[:data].each do |item|
-        
-        # expect(merchant).to have_key(:id)
-        #how can i test the key?
-        # expect(merchant[:attributes][:id]).to be_an(Integer)
-        
+        binding.pry
         expect(item[:attributes]).to have_key(:name)
         expect(item[:attributes][:name]).to be_a(String)
-        
         expect(item[:attributes]).to have_key(:description)
         expect(item[:attributes][:description]).to be_a(String)
-        
         expect(item[:attributes]).to have_key(:unit_price)
-        # expect(item[:attributes][:unit_price]).to be_a(Integer)
       end 
     end 
     
